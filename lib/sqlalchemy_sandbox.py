056beef25335591dfe823c679a659d3371d8cf6b
omarfarouq ahmed <omarfarouq.ahmed@student.moringaschool.com>
2023-08-31 15:46:05 +0300

    done

--- lib/sqlalchemy_sandbox.py
+++ lib/sqlalchemy_sandbox.py
@@ -1,3 +1,4 @@
+
 #!/usr/bin/env python3
 
 from datetime import datetime
@@ -12,10 +13,97 @@ Base = declarative_base()
 
 class Student(Base):
     __tablename__ = 'students'
+    __table_args__ = (
+        PrimaryKeyConstraint(
+            'id',
+            name='id_pk'),
+        UniqueConstraint(
+            'email',
+            name='unique_email'),
+        CheckConstraint(
+            'grade BETWEEN 1 AND 12',
+            name='grade_between_1_and_12')
+    )
 
-    id = Column(Integer(), primary_key=True)
+    Index('index_name', 'name')
+
+    id = Column(Integer())
     name = Column(String())
+    email = Column(String(55))
+    grade = Column(Integer())
+    birthday = Column(DateTime())
+    enrolled_date = Column(DateTime(), default=datetime.now())
+
+    def __repr__(self):
+        return f"Student {self.id}: " \
+            + f"{self.name}, " \
+            + f"Grade {self.grade}"
 
 if __name__ == '__main__':
     engine = create_engine('sqlite:///:memory:')
     Base.metadata.create_all(engine)
+
+
+# Create a session
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    albert_einstein = Student(
+        name="Albert Einstein",
+        email="albert.einstein@zurich.edu",
+        grade=6,
+        birthday=datetime(
+            year=1879,
+            month=3,
+            day=14
+        ),
+    )
+
+    alan_turing = Student(
+        name="Alan Turing",
+        email="alan.turing@sherborne.edu",
+        grade=11,
+        birthday=datetime(
+            year=1912,
+            month=6,
+            day=23
+        ),
+    )
+
+    session.bulk_save_objects([albert_einstein, alan_turing])
+    session.commit()
+
+    print(f"New student ID is {albert_einstein.id}.")
+    print(f"New student ID is {alan_turing.id}.")
+
+    # student_count = session.query(func.count(Student.id)).first()
+
+    # print(student_count)
+
+    # names = [name for name in session.query(Student.name)]
+
+    # print(names)
+
+
+    # # Add a student record
+    # new_student = Student(name='John Doe')
+    # session.add(new_student)
+    # session.commit()
+
+    # # Query the database to retrieve the added student
+    # retrieved_student = session.query(Student).filter_by(name='John Doe').first()
+    # if retrieved_student:
+    #     print(f"Retrieved student: {retrieved_student.id}, {retrieved_student.name}")
+    # else:
+    #     print("Student not found")
+
+
+
+
+
+
+
+
+
+
+
